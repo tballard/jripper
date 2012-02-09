9c5312d5060edd9a9339bd0ee07ae34260b68fc1
Tom Ballard <tom@geeksite.org>
2012-02-09 09:53:58 -0700

    move annotation class

--- jripper_demo/src/srcbin/jripper_demo/package-info.java
+++ jripper_demo/src/srcbin/jripper_demo/package-info.java
@@ -1,6 +1,6 @@
 @JRipper(dirName = Info.DIR)    // This is how you annotate a package.
 package srcbin.jripper_demo;
-import srcbin.jripper.JRipper;  // Import the code ripper
+import srcbin.jripper.annotation.JRipper;  // Import the code ripper
 
 class Info {
    /** This is the directory to throw the code blocks in. */
