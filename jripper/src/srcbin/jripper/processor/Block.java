9a45802445babcb5893fadb5a7b40e76a7863b44
Tom Ballard <tom@geeksite.org>
2012-02-09 09:39:39 -0700

    move functionality to Block

--- jripper/src/srcbin/jripper/processor/Block.java
+++ jripper/src/srcbin/jripper/processor/Block.java
@@ -97,4 +97,19 @@ public class Block {
       }
       return len;
    }
+   
+   /**
+    * Retrieves the text that you asked to be ripped 
+    * @param dirName The directory used in package-info.java
+    * @param blockName The name from the block header
+    */
+   public static Block[] get(String dirName, String blockName) {
+     System.out.println(System.getProperty("java.class.path"));
+      try {
+         Class clazz = Class.forName(dirName + '.' + blockName);
+         return (Block[])clazz.getField("SRC").get(clazz);
+      } catch (Exception ex) {
+         throw new RuntimeException(ex);
+      }
+   }
 }
